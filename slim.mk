9156b295f9a00d4197a61a0545136d86c2b16c3c
Ziyan <jaraidaniel@gmail.com>
2016-02-21 17:03:37 +0100

    Common device tree for 3G variants
    Change-Id: I957fac5b5d41b4afac158fb59feb4e45e2bcd0cf

--- slim.mk
+++ slim.mk
@@ -15,29 +15,20 @@
 # limitations under the License.
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := p5100
-
 # Boot animation
-TARGET_SCREEN_HEIGHT := 800
-TARGET_SCREEN_WIDTH := 1280
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 600
 
 # Inherit common Slim configuration
 $(call inherit-product, vendor/slim/config/gsm.mk)
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # SlimRoms specific overlay
-DEVICE_PACKAGE_OVERLAYS += device/samsung/p5100/overlay/slim
-DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso-common/overlay/slim-common
+DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso3g/overlay/slim
+DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso/overlay/slim-common
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/p5100/aosp_p5100.mk)
+$(call inherit-product, device/samsung/espresso3g/aosp_espresso3g.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := slim_p5100
-
-# Set build fingerprint / ID / Product Name etc.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="samsung/espresso10rfxx/espresso10rf:4.2.2/JDQ39/P5100XXDNA1:user/release-keys" \
-    PRIVATE_BUILD_DESC="espresso10rfxx-user 4.2.2 JDQ39 P5100XXDNA1 release-keys"
-
+PRODUCT_NAME := slim_espresso3g
