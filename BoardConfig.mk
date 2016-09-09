03a0b7a7242bbb36a945c8b234f96792696f6312
Ziyan <jaraidaniel@gmail.com>
2016-09-09 13:48:59 +0200

    Remove custom libril
    Change-Id: I561608662bc207a208da81d461a2532ca0a2f5af

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,10 +18,5 @@
 include device/samsung/espressowifi/BoardConfigCommon.mk
 
 
-TARGET_SPECIFIC_HEADER_PATH += device/samsung/espresso3g/include
-
 # assert
 TARGET_OTA_ASSERT_DEVICE := espresso3g,p3100,GT-P3100,espressorf,espressorfxx,p5100,GT-P5100,espresso10rf,espresso10rfxx,espresso-common
-
-# RIL
-BOARD_PROVIDES_LIBRIL := true
