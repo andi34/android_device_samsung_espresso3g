086f968904d50871a4725c26053aab6f801a6b14
Andreas Blaesius <skate4life@gmx.de>
2017-10-22 00:29:28 +0200

    espresso3g: rename slim -> custom
    Change-Id: I9cf07724017a17ece564e175969b55fddb18c37c

--- slim.mk
+++ slim.mk
@@ -24,8 +24,8 @@ TARGET_SCREEN_WIDTH := 1280
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # SlimRoms specific overlay
-DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso3g/overlay/slim
-DEVICE_PACKAGE_OVERLAYS += device/samsung/espressowifi/overlay/slim-common
+DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso3g/overlay/custom
+DEVICE_PACKAGE_OVERLAYS += device/samsung/espressowifi/overlay/custom-common
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/espresso3g/aosp_espresso3g.mk)
